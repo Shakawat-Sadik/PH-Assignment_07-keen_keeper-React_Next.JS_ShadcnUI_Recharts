efb603638b08e5e0a42b61a921ae88072e40a657
Shakawat Hossain Sadik <saadiq9x@gmail.com>
2026-05-11 08:27:38 +0600

    Project Completed. Fixed some productivity bugs only.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,5 @@
-"use client"
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "./ui/navigation-menu";
+"use client";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "./ui/navigation-menu";
 import logo from "../assets/logo.png";
 import Image from "next/image";
 import Link from "next/link";
@@ -13,13 +9,20 @@ import { ChartDonutIcon, ClockAfternoonIcon, ClockClockwiseIcon, HouseLineIcon, 
 
 const Navbar = () => {
   const path = usePathname();
-  
+
   return (
     <div className="sticky top-0 z-40 bg-background/70 flex flex-row w-full">
       <NavigationMenu className="flex justify-between">
         <NavigationMenuList>
           <NavigationMenuItem>
-                 <Image loading="eager" src={logo} alt="Logo" width={200} height={80} className="w-auto h-auto dark:drop-shadow-[0rem_0_0.1rem_#fff] shrink-0"/>
+            <Image
+              loading="eager"
+              src={logo}
+              alt="Logo"
+              width={200}
+              height={80}
+              className="w-auto h-auto dark:drop-shadow-[0rem_0_0.1rem_#fff] shrink-0"
+            />
           </NavigationMenuItem>
         </NavigationMenuList>
 
@@ -29,7 +32,7 @@ const Navbar = () => {
               href="/"
               className={`${path === "/" ? "bg-primary text-background hover:text-(--hover-primary) scale-105" : "hover:bg-(--hover-primary) hover:text-foreground"} flex items-center gap-2 rounded-sm p-2 font-medium transition-all duration-300 outline-none focus-visible:ring-3 focus-visible:ring-ring/50 cursor-pointer`}
             >
-            <HouseLineIcon size={16} />
+              <HouseLineIcon size={16} />
               Home
             </Link>
           </NavigationMenuItem>
@@ -38,7 +41,7 @@ const Navbar = () => {
               href="/timeline"
               className={`${path === "/timeline" ? "bg-primary text-background hover:text-(--hover-primary) scale-105" : "hover:bg-(--hover-primary) hover:text-foreground"} inline-flex items-center gap-2 rounded-sm p-2 font-medium transition-all duration-300 outline-none focus-visible:ring-3 focus-visible:ring-ring/50 cursor-pointer`}
             >
-            <ClockClockwiseIcon size={16} />
+              <ClockClockwiseIcon size={16} />
               Timeline
             </Link>
           </NavigationMenuItem>
@@ -47,7 +50,7 @@ const Navbar = () => {
               href="/stats"
               className={`${path === "/stats" ? "bg-primary text-background hover:text-(--hover-primary) scale-105" : "hover:bg-(--hover-primary) hover:text-foreground"} inline-flex items-center gap-2 rounded-sm p-2 font-medium transition-all duration-300 outline-none focus-visible:ring-3 focus-visible:ring-ring/50 cursor-pointer`}
             >
-            <ChartDonutIcon size={16} />
+              <ChartDonutIcon size={16} />
               Stats
             </Link>
           </NavigationMenuItem>
